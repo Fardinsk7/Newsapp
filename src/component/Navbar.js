abb1f68367530ed6b4763f6854ade7749282a3a6
Fardinsk7 <itzfardinsk@gmail.com>
2023-05-21 23:20:18 +0530

    News app Final

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,54 +1,75 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default class Navbar extends Component {
   render() {
     return (
       <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">Nutshell News</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link" aria-current="page" to="/">Home</Link>
-        </li>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">Nutshell News</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                
 
-        <li className="nav-item">
-          <Link className="nav-link" to="/">General</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/business">Business</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/technology">Technology</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/science">Science</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/sports">Sports</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/entertainment">Entertainment</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/health">Health</Link>
-        </li>
-      
-       
-      </ul>
-      {/* <form className="d-flex" role="search">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">General</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/business">Business</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/technology">Technology</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/science">Science</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/sports">Sports</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/entertainment">Entertainment</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/health">Health</Link>
+                </li>
+
+
+              </ul>
+              {/* <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Search</button>
       </form> */}
-    </div>
-  </div>
-</nav>
-        
+              <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Change Country
+              </button>
+
+              <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                      <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                      ...
+                    </div>
+                    <div className="modal-footer">
+                      <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <button type="button" className="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+        </nav>
+
       </>
     )
   }
--- src/component/News.js
+++ src/component/News.js
@@ -3,6 +3,8 @@ import NewsItems from './NewsItems'
 import "./style.css"
 import noimage from "./noimage.jpg"
 import Spinner from './Spinner'
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 export default class News extends Component {
     static defaultProps = {
@@ -12,12 +14,13 @@ export default class News extends Component {
     }
 
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             articles: [],
             loading: false,
             page: 1,
+            totalResult:0
         }
     }
 
@@ -33,28 +36,39 @@ export default class News extends Component {
             totalResult: parsedData.totalResults,
             loading: false
         })
+        console.log(this.state.totalResult)
     }
 
 
     async componentDidMount() {
         this.updateNews(this.state.page)
-        console.log(Math.ceil((this.state.totalResult / 10)))
-        
     }
 
-    Previous= async ()=>{
-        this.setState({page:this.state.page-1})
-        this.updateNews(this.state.page)
-        
-        console.log("previous",this.state.page)
+    Previous= ()=>{
+        this.setState({page:this.state.page -1})
+        this.updateNews(this.state.page-1)        
     }
     
-    Next=()=>{
-        this.setState({page:this.state.page+1})
-        this.updateNews(this.state.page)
-        console.log('next',this.state.page)
+    Next= ()=>{ 
+        this.setState({page:this.state.page +1})
+        this.updateNews(this.state.page+1)
     }
 
+    fetchMoreData = async()=>{
+        this.setState({page:this.state.page + 1})
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a3804b71fea845a7aa477d2abf0d83c7&pageSize=10&page=${this.state.page+1}`;
+
+        const data = await fetch(url)
+        this.setState({ loading: true })
+        let parsedData = await data.json()
+        // console.log(parsedData)
+
+        this.setState({
+            articles: this.state.articles.concat(parsedData.articles),
+            totalResult: parsedData.totalResults,
+            loading: false,
+        }) 
+    }
 
     render() {
 
@@ -62,9 +76,23 @@ export default class News extends Component {
         return (
             <>
                 <h1 style={{ textAlign: "center", margin: "20px", textDecoration: "underline" }}>Top News on {this.props.category}</h1>
-                {this.state.loading && <Spinner />}
+
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    // style={{ display: 'flex', flexDirection: 'column-reverse' }} //To put endMessage and loader to the top.
+                    hasMore={this.state.articles.length !== this.state.totalResult}//It is true then it means that data is still remain to load
+                    loader={<Spinner/>}
+                    endMessage={
+                        <p style={{ textAlign: 'center' }}>
+                          <b>Yay! You have seen it all</b>
+                        </p>
+                      }
+                >
+
+                {/* {this.state.loading && <Spinner />} */}
                 <div className="container News-container">
-                    {this.state.articles.map((element) => {
+                    {!this.state.loading && this.state.articles.map((element) => {
                         return <div key={element.url}>
                             <NewsItems title={element.title} description={element.description} image={element.urlToImage === null ? noimage : element.urlToImage} url={element.url} />
                         </div>
@@ -74,10 +102,12 @@ export default class News extends Component {
                     }
 
                 </div>
-                <div className="container  d-flex justify-content-between p-2" >
+                </InfiniteScroll>
+
+                {/* <div className="container  d-flex justify-content-between p-2" >
                     <button className="btn btn-primary" disabled={this.state.page<=1}  onClick={this.Previous} >&larr; Previous</button>
                     <button className="btn btn-primary" disabled={this.state.page+1>= Math.ceil((this.state.totalResult/10))} onClick={this.Next}>Next &rarr; </button>
-                </div>
+                </div> */}
             </>
         )
     }
